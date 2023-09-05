d823b7b2002593a62616d9991f59598100dc6cd5
Ihor Mruchko <55592478+IhorMruchko@users.noreply.github.com>
2023-09-05 22:06:17 +0300

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,7 @@
+﻿using System.Windows;
+
+namespace ThemeCreator;
+
+public partial class App : Application
+{
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace ThemeCreator;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
